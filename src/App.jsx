46c05a5c940c007e5fbc09b2ffd87dbd58da8c64
Sathwik12345678 <goudsathwik55@gmail.com>
2026-02-08 00:21:24 +0530

    Final GitHub Pages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import Resume from "./pages/Resume"
-import Contact from "./pages/Contact"
-import { Routes, Route, useLocation } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
 
 function App() {
-  const location = useLocation()
-
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-[#0b0f19] text-black dark:text-white">
-      
-      {/* Show Navbar only if NOT Home */}
-      {location.pathname !== "/" && <Navbar />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
